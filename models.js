a61c8ac8e7694b8fb5b79a8e19f252147767477b
Mostafa Ahangarha <ahangarha@gmail.com>
2019-01-31 13:21:13 +0330

    replace tabs with double spaces

--- models.js
+++ models.js
@@ -32,80 +32,80 @@ class Post extends BaseModel{
 };
 
 class User extends BaseModel{
-	constructor(){
-		super();
+  constructor(){
+    super();
 
-		this.addField('firstName', inn.STRING(64));
-		this.addField('lastName', inn.STRING(64));
-		this.addField('email', inn.STRING(64));
-		this.addField('password', inn.STRING(128));
-	}
+    this.addField('firstName', inn.STRING(64));
+    this.addField('lastName', inn.STRING(64));
+    this.addField('email', inn.STRING(64));
+    this.addField('password', inn.STRING(128));
+  }
 }
 
 class Roles extends BaseModel{
-	constructor(){
-		super();
+  constructor(){
+    super();
 
-		this.addField('name', inn.STRING(64));
-		this.addField('desctiption', inn.STRING(128));
-	}
+    this.addField('name', inn.STRING(64));
+    this.addField('desctiption', inn.STRING(128));
+  }
 }
 
 class UserRoles extends BaseModel{
-	constructor(){
-		super();
+  constructor(){
+    super();
 
-		this.addField('userId', inn.relation.foreignkey(inn.User));
-		this.addField('roleId', inn.relation.foreignkey(inn.Roles));
-	}
+    this.addField('userId', inn.relation.foreignkey(inn.User));
+    this.addField('roleId', inn.relation.foreignkey(inn.Roles));
+  }
 }
 
 class Comments extends BaseModel{
-	constructor(){
-		super();
+  constructor(){
+    super();
 
-		this.addField('userId', inn.relation.foreignkey(inn.User));
-		this.addField('postId', inn.relation.foreignkey(inn.Post));
-		this.addField('text', inn.STRING(1024));
-	}
+    this.addField('userId', inn.relation.foreignkey(inn.User));
+    this.addField('postId', inn.relation.foreignkey(inn.Post));
+    this.addField('text', inn.STRING(1024));
+  }
 }
 
 
 class Categories extends BaseModel{
-	constructor(){
-		super();
+  constructor(){
+    super();
 
-		this.addField('type', inn.STRING(64));
-		this.addField('desctiption', inn.STRING(128));
-	}
+    this.addField('type', inn.STRING(64));
+    this.addField('desctiption', inn.STRING(128));
+  }
 }
 
 class CategoriesPost extends BaseModel{
-	constructor(){
-		super();
+  constructor(){
+    super();
 
-		this.addField('categoryId', inn.relation.foreignkey(inn.Categories));
-		this.addField('postId', inn.relation.foreignkey(inn.Post));
-	}
+    this.addField('categoryId', inn.relation.foreignkey(inn.Categories));
+    this.addField('postId', inn.relation.foreignkey(inn.Post));
+  }
 }
 
 class PostLikes extends BaseModel{
-	constructor(){
-		super();
+  constructor(){
+    super();
 
-		this.addField('userId', inn.relation.foreignkey(inn.User));
-		this.addField('postId', inn.relation.foreignkey(inn.Post));
+    this.addField('userId', inn.relation.foreignkey(inn.User));
+    this.addField('postId', inn.relation.foreignkey(inn.Post));
     this.addField('createdAt', inn.DATETIME);
-	}
+  }
 }
 module.exports = {
   Blog,
   Post,
-	User,
-	Roles,
-	UserRoles,
-	Comments,
-	Categories,
-	CategoriesPost,
-	PostLikes,
+  User,
+  Roles,
+  UserRoles,
+  Comments,
+  Categories,
+  CategoriesPost,
+  PostLikes,
 }
