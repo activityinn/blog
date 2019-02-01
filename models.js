7901ce6a2d0b0f8f394272b87bb2ed11f2a63a09
Mostafa Ahangarha <ahangarha@users.noreply.github.com>
2019-02-01 19:34:19 +0330

    Added more models
    Added more models

--- models.js
+++ models.js
@@ -27,11 +27,67 @@ class Post extends BaseModel{
 
     this.addField('blog', models.relation.foreignkey(Blog));
 
+		this.addField('comment', models.relation.foreignkey(Comment)); 
+
     this.setBaseName('post');
   }
 };
 
+class Comment extends BaseModel{
+  constructor(){
+    super();
+
+    this.addField('userId', models.relation.foreignkey(models.Actor));
+
+		// TODO:
+		// comment can be made under a post or under another comment.
+		// Moreover, commets can be handled like a mastodon toot.
+		// Needs to be strudied further.
+    this.addField('postId', models.relation.foreignkey(Post));
+		
+		// TODO: decide about length of comment
+		this.addField('text', models.STRING(1024));
+  }
+}
+
+
+class Category extends BaseModel{
+  constructor(){
+    super();
+
+    this.addField('type', models.STRING(64));
+    this.addField('desctiption', models.STRING(128));
+  }
+}
+
+class CategoryPost extends BaseModel{
+  constructor(){
+    super();
+
+    this.addField('categoryId', models.relation.foreignkey(Category));
+    this.addField('postId', models.relation.foreignkey(Post));
+  }
+}
+
+class PostLike extends BaseModel{
+  constructor(){
+    super();
+
+		// TODO:
+		// Liking a post would be completely similar to liking a Mastodon toot.
+		// Needs to be studied further
+
+    this.addField('userId', models.relation.foreignkey(models.Actor));
+    this.addField('postId', models.relation.foreignkey(Post));
+    this.addField('createdAt', models.DATETIME);
+  }
+}
+
 module.exports = {
   Blog,
   Post,
+  Comment,
+  Category,
+  CategoryPost,
+  PostLike,
 }
